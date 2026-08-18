6ac548fb01126c52a9cce2633a7fb8d215cdc88c
husain jamil khan <zk3344804@gmail.com>
2026-08-18 12:49:58 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,6 @@
+a=30
+b=60
+print("the value of",a,"+",3," is :",a+b)
+print("the value of",a,"-",3," is :",a-b)
+print("the value of",a,"*",3," is :",a*b)
+print("the value of",a,"/",3,"*is :",a/b)
